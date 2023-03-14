fc0f7eb48e8c69c84de4c339e2d569ae9b99b2a5
Alena Kiseleva <kiseleva_elena92@mail.ru>
2023-03-14 14:03:06 +0400

    edit cancel ordering method

--- src/main/java/com/guavapay/delivery/entity/enums/DeliveryStatus.java
+++ src/main/java/com/guavapay/delivery/entity/enums/DeliveryStatus.java
@@ -3,6 +3,7 @@ package com.guavapay.delivery.entity.enums;
 public enum DeliveryStatus {
 
     STATUS_ACCEPTED,
+    STATUS_CANCELED,
     STATUS_DELIVERED
 
 }
--- src/main/java/com/guavapay/delivery/service/impl/OrderingServiceImpl.java
+++ src/main/java/com/guavapay/delivery/service/impl/OrderingServiceImpl.java
@@ -5,6 +5,7 @@ import com.guavapay.delivery.dto.response.OrderingResponse;
 import com.guavapay.delivery.entity.Item;
 import com.guavapay.delivery.entity.Ordering;
 import com.guavapay.delivery.entity.UserData;
+import com.guavapay.delivery.entity.enums.DeliveryStatus;
 import com.guavapay.delivery.entity.enums.OrderingStatus;
 import com.guavapay.delivery.exception.ItemAlreadyTakenException;
 import com.guavapay.delivery.helper.OrderingHelper;
@@ -55,7 +56,10 @@ public class OrderingServiceImpl implements OrderingService {
     @Transactional
     public OrderingResponse cancelOrdering(Long id) {
         Ordering ordering = orderingHelper.findOrderingById(id);
+        checkUserAccess(ordering.getUser());
         ordering.setOrderingStatus(OrderingStatus.STATUS_CANCELLED);
+        if (ordering.getDelivery() != null)
+            ordering.getDelivery().setDeliveryStatus(DeliveryStatus.STATUS_CANCELED);
         Ordering savedOrdering = orderingRepository.save(ordering);
         log.info("Ordering with id {} canceled", id);
         return orderingMapper.mapToResponse(savedOrdering);
@@ -64,7 +68,7 @@ public class OrderingServiceImpl implements OrderingService {
     void checkUserAccess(UserData userData) {
         UserData currentUser = userHelper.getCurrentUserData();
         if (!currentUser.equals(userData))
-            throw new AccessDeniedException(String.format("User with id %d cant get access to ordering", currentUser.getId()));
+            throw new AccessDeniedException(String.format("User with id %d cant get access to this ordering", currentUser.getId()));
     }
 
     private void checkItem(Item item) {
